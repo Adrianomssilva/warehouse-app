8a3585ded9651f1e82244a34871745bc501cf026
devadrsilva <adriano.mssilva1@gmail.com>
2023-10-19 14:32:12 -0300

    show criada

---
+++ spec/system/user_view_warehouse_description_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "Usuários vê detalhe de galpão" do
+    it "e vê informações adicionais" do
+      # Arrange
+      Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000, address: 'Avenida do Aeroporto, 1000', cep: '15000-000', description: 'Galpão destinado a cargas internacionais' )
+      #Act
+      visit('/')
+      click_on('Aeroporto SP')
+
+      #Assert
+      expect(page).to have_content('Galpão GRU')
+      expect(page).to have_content('Nome: Aeroporto SP')
+      expect(page).to have_content('Cidade: Guarulhos')
+      expect(page).to have_content('Área: 100000 m²')
+      expect(page).to have_content('Endereço: Avenida do Aeroporto, 1000')
+      expect(page).to have_content('CEP: 15000-000')
+      expect(page).to have_content('Galpão destinado a cargas internacionais')
+    end
+end

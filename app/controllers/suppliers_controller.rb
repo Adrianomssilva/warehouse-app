ab083b9ef9c0e157bc5895bacea8f55f2018a18d
devadrsilva <adriano.mssilva1@gmail.com>
2023-10-24 16:37:30 -0300

    create and new criados

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -8,4 +8,21 @@ class SuppliersController < ApplicationController
   def show
     @supplier = Supplier.find(params[:id])
   end
+
+  def new
+    @supplier = Supplier.new
+  end
+
+  def create
+    @supplier = Supplier.new(suppliers_params)
+    @supplier.save
+    redirect_to suppliers_path, notice: 'Fornecedor cadastrado com sucesso!'
+  end
+
+
+  private
+
+  def suppliers_params
+    params.require(:supplier).permit(:corporate_name, :brand_name, :registration_number, :full_address, :city, :state, :email)
+  end
 end
--- spec/system/supplier/user_register_supplier_spec.rb
+++ spec/system/supplier/user_register_supplier_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "Usuário cria fornecedore" do
+describe "Usuário cria fornecedor" do
   it "a partir da tela inicial" do
 
     #Arrange
@@ -10,7 +10,39 @@ describe "Usuário cria fornecedore" do
     click_on 'Fornecedores'
     click_on 'Novo Fornecedor'
     #Assert
-    expect(page).to have_field ''
+    expect(page).to have_field 'Razão Social'
+    expect(page).to have_field 'Nome Fantasia'
+    expect(page).to have_field 'CNPJ'
+    expect(page).to have_field 'Endereço'
+    expect(page).to have_field 'Cidade'
+    expect(page).to have_field 'Estado'
+    expect(page).to have_field 'E-mail'
+
+  end
+
+  it "com sucesso" do
+    # Arrange
+    #
+    #Act
+    visit root_path
+    click_on 'Fornecedores'
+    click_on 'Novo Fornecedor'
+
+    fill_in "Razão Social",	with: "Nestle S/A"
+    fill_in "Nome Fantasia",	with: "Nestle"
+    fill_in "CNPJ",	with: "203010/0001-40"
+    fill_in "Endereço",	with: "Rua das industrias, 120"
+    fill_in "Cidade",	with: "Sorocaba"
+    fill_in "Estado",	with: "SP"
+    fill_in "E-mail",	with: "contato@nestle.com.br"
+    click_on 'Enviar'
+
+    # Assert
+    expect(current_path).to eq suppliers_path
+    expect(page).to have_content 'Fornecedor cadastrado com sucesso!'
+    expect(page).to have_content 'Nestle'
+    expect(page).to have_content 'Sorocaba'
+
 
   end
 

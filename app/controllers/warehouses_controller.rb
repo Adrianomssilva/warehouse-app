9357c06b13ae73b20737080dba6c64e1fc20dd6b
devadrsilva <adriano.mssilva1@gmail.com>
2023-10-24 11:22:28 -0300

    crud done

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,6 +1,6 @@
 class WarehousesController < ApplicationController
 
-  before_action :set_warehouse, only: [:edit, :update, :show]
+  before_action :set_warehouse, only: [:edit, :update, :show, :destroy]
 
   def show
   end
@@ -29,6 +29,11 @@ class WarehousesController < ApplicationController
       flash.now[:notice] = 'Não foi possível editar o galpão'
       render 'edit'
     end
+  end
+
+  def destroy
+    @warehouse.destroy
+    redirect_to root_path, notice: 'Galpão removido com sucesso'
   end
 
   private
---
+++ spec/system/user_delete_warehouse_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe "Usuário remove um galpão" do
+
+  it "com sucesso" do
+    # Arrange
+    Warehouse.create!(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000, address: 'Aeroporto do Galeão', cep: '20000-100', description: 'galpão localizado no aeroporto do galeão')
+    #Act
+    visit root_path
+    click_on 'Rio'
+    click_on 'Remover'
+    #Assert
+      expect(current_path).to eq root_path
+      expect(page).to have_content 'Galpão removido com sucesso'
+      expect(page).not_to have_content 'Rio'
+      expect(page).not_to have_content 'SDU'
+
+  end
+  it "apenas o galpão correto é apagado" do
+    # Arrange
+    Warehouse.create!(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000, address: 'Aeroporto do Galeão', cep: '20000-100', description: 'galpão localizado no aeroporto do galeão')
+    Warehouse.create!(name: 'Sp', code: 'SPG', city: 'São Paulo', area: 100_000, address: 'Grande São Paulo, 200', cep: '12000-100', description: 'galpão localizado no centro de SP')
+    #Act
+    visit root_path
+    click_on 'Rio'
+    click_on 'Remover'
+    #Assert
+      expect(current_path).to eq root_path
+      expect(page).to have_content 'Galpão removido com sucesso'
+      expect(page).not_to have_content 'Rio'
+      expect(page).to have_content 'Sp'
+
+  end
+
+
+end

36342072a180ff8267df6c03b0e801c38872220e
devadrsilva <adriano.mssilva1@gmail.com>
2023-10-24 10:12:25 -0300

    clean code ajustado

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,7 +1,8 @@
 class WarehousesController < ApplicationController
 
+  before_action :set_warehouse, only: [:edit, :update, :show]
+
   def show
-    @warehouse = Warehouse.find(params[:id])
   end
 
   def new
@@ -9,7 +10,6 @@ class WarehousesController < ApplicationController
   end
 
   def create
-    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area)
     @warehouse = Warehouse.new(warehouse_params)
     if @warehouse.save
     redirect_to root_path, notice: 'Galpão cadastrado com sucesso'
@@ -20,18 +20,25 @@ class WarehousesController < ApplicationController
   end
 
   def edit
-    @warehouse = Warehouse.find(params[:id])
   end
 
   def update
-    @warehouse = Warehouse.find(params[:id])
-    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area)
     if @warehouse.update(warehouse_params)
     redirect_to warehouse_path, notice: 'Galpão editado com sucesso!'
     else
       flash.now[:notice] = 'Não foi possível editar o galpão'
       render 'edit'
     end
+  end
+
+  private
+
+  def set_warehouse
+    @warehouse = Warehouse.find(params[:id])
+  end
+
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area)
   end
 
 end
